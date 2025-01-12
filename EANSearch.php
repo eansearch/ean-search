02ac82cf5cb3469aa8202038b3adbe033a09b2ec
Jan Willamowius <jan@willamowius.de>
2025-01-12 21:58:35 +0100

    declare method visibility

--- EANSearch.php
+++ EANSearch.php
@@ -16,15 +16,15 @@ class EANSearch {
 	private const BASE_URL = 'https://api.ean-search.org/api?format=xml';
 	private const MAX_API_TRIES = 3;
 
-	function __construct($accessToken) {
+	public function __construct($accessToken) {
 		$this->accessToken = $accessToken;
 		$this->setTimout(180);
 	}
 
 	/// look up one EAN / GTIN / UPC barcode
 	/// only return the product name
-	function barcodeLookup($ean, $lang = 1) {
-		$xml = $this->_apiCall("op=barcode-lookup&ean=$ean&language=$lang");
+	public function barcodeLookup($ean, $lang = 1) {
+		$xml = $this->apiCall("op=barcode-lookup&ean=$ean&language=$lang");
 		if ($xml === FALSE) {
 			return '';
 		}
@@ -34,8 +34,8 @@ class EANSearch {
 
 	/// look up one EAN / GTIN / UPC barcode
 	/// return all product data
-	function barcodeSearch($ean, $lang = 1) {
-		$xml = $this->_apiCall("op=barcode-lookup&ean=$ean&language=$lang");
+	public function barcodeSearch($ean, $lang = 1) {
+		$xml = $this->apiCall("op=barcode-lookup&ean=$ean&language=$lang");
 		if ($xml === FALSE) {
 			return array();
 		}
@@ -45,8 +45,8 @@ class EANSearch {
 
 	/// look up one ISBN-10 code
 	/// only return the book name
-	function isbnLookup($isbn) {
-		$xml = $this->_apiCall("op=barcode-lookup&isbn=$isbn");
+	public function isbnLookup($isbn) {
+		$xml = $this->apiCall("op=barcode-lookup&isbn=$isbn");
 		if ($xml === FALSE) {
 			return '';
 		}
@@ -55,8 +55,8 @@ class EANSearch {
 	}
 
 	/// get all barcodes below a prefix
-	function barcodePrefixSearch($prefix, $page = 0) {
-		$xml = $this->_apiCall("op=barcode-prefix-search&prefix=$prefix&page=$page");
+	public function barcodePrefixSearch($prefix, $page = 0) {
+		$xml = $this->apiCall("op=barcode-prefix-search&prefix=$prefix&page=$page");
 		if ($xml === FALSE) {
 			return array();
 		}
@@ -65,9 +65,9 @@ class EANSearch {
 	}
 
 	/// search for a product name or keyword (exact match)
-	function productSearch($name, $page = 0) {
+	public function productSearch($name, $page = 0) {
 		$name = urlencode($name);
-		$xml = $this->_apiCall("op=product-search&name=$name&page=$page");
+		$xml = $this->apiCall("op=product-search&name=$name&page=$page");
 		if ($xml === FALSE) {
 			return array();
 		}
@@ -76,9 +76,9 @@ class EANSearch {
 	}
 
 	/// search for a product name or keyword (get similar matches)
-	function similarProductSearch($name, $page = 0) {
+	public function similarProductSearch($name, $page = 0) {
 		$name = urlencode($name);
-		$xml = $this->_apiCall("op=similar-product-search&name=$name&page=$page");
+		$xml = $this->apiCall("op=similar-product-search&name=$name&page=$page");
 		if ($xml === FALSE) {
 			return array();
 		}
@@ -87,9 +87,9 @@ class EANSearch {
 	}
 
 	/// search for a product name or keyword in a product category (exact match)
-	function categorySearch($category, $name = '', $page = 0) {
+	public function categorySearch($category, $name = '', $page = 0) {
 		$name = urlencode($name);
-		$xml = $this->_apiCall("op=category-search&category=$category&name=$name&page=$page");
+		$xml = $this->apiCall("op=category-search&category=$category&name=$name&page=$page");
 		if ($xml === FALSE) {
 			return array();
 		}
@@ -98,51 +98,51 @@ class EANSearch {
 	}
 
 	/// generate a PNG barcode image
-	function barcodeImage($ean, $width = 102, $height = 50) {
-		$xml = $this->_apiCall("op=barcode-image&ean=$ean&width=$width&height=$height");
+	public function barcodeImage($ean, $width = 102, $height = 50) {
+		$xml = $this->apiCall("op=barcode-image&ean=$ean&width=$width&height=$height");
 		$response = new SimpleXMLElement($xml);
 		return base64_decode($response->product->barcode);
 	}
 
 	/// verify the checksum of a EAN / GTIN / UPC / ISBN-13 barcode
-	function verifyChecksum($ean) {
-		$xml = $this->_apiCall("op=verify-checksum&ean=$ean");
+	public function verifyChecksum($ean) {
+		$xml = $this->apiCall("op=verify-checksum&ean=$ean");
 		$response = new SimpleXMLElement($xml);
 		return $response->product->valid;
 	}
 
 	/// get the issuing country of any EAN / GTIN / UPC / ISBN-13 barcode
-	function issuingCountryLookup($ean) {
-		$xml = $this->_apiCall("op=issuing-country&ean=$ean");
+	public function issuingCountryLookup($ean) {
+		$xml = $this->apiCall("op=issuing-country&ean=$ean");
 		$response = new SimpleXMLElement($xml);
 		return $response->product->issuingCountry;
 	}
 
 	/// get remaining API credits
 	/// returns -1 before first API call
-	function creditsRemaining() {
+	public function creditsRemaining() {
 		return $this->remaining;
 	}
 
 	/// set HTTP timeout in seconds
-	function setTimout($sec) {
+	public function setTimout($sec) {
 		$this->ctx = stream_context_create(array('http' => array('timeout' => 180, 'ignore_errors' => true)));
 		ini_set('default_socket_timeout', $sec);
 	}
 
-	function _apiCall($params, $tries = 1) {
+	protected function apiCall($params, $tries = 1) {
 		$xml = file_get_contents(self::BASE_URL . "&token=$this->accessToken&" . $params, false, $this->ctx);
 		 foreach($http_response_header as $k=>$v) {
-			$h = explode(':', $v, 2);
-			if($h[0] == 'X-Credits-Remaining') {
-				$this->remaining = trim($h[1]);
+			$head = explode(':', $v, 2);
+			if($head[0] == 'X-Credits-Remaining') {
+				$this->remaining = trim($head[1]);
 			}
 		}
 		preg_match('{HTTP\/\S*\s(\d{3})}', $http_response_header[0], $match);
 		$status = $match[1];
 		if ($status == 429 && $tries < self::MAX_API_TRIES) {
             sleep(1);
-			return _apiCall($params, tries+1);
+			return apiCall($params, tries+1);
 		}
 		return $xml;
 	}
